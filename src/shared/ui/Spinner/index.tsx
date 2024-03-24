cfefcd957affd57cdd05a4a6ba1b710541e36a25
finf0094 <finf0094@gmail.com>
2024-03-24 20:48:23 +0500

    feat: shared.ui(spinner)

---
+++ src/shared/ui/Spinner/index.tsx
@@ -0,0 +1,15 @@
+import React, { type FC } from 'react';
+
+import { classNames } from 'shared/lib/classNames';
+
+import cls from './Spinner.module.scss';
+
+interface SpinnerProps {
+    className?: string
+}
+
+const Spinner: FC<SpinnerProps> = ({ className }) => (
+    <div className={classNames(cls.loader, {}, [className])} />
+);
+
+export default Spinner;
